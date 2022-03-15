e510d165554f5421857ee3e392acd9ccdd22c01f
Sumit Kumar <sumitskl1999@gmail.com>
2022-03-15 11:59:54 +0530

    refractored and completed

--- js/onButtonClick.js
+++ js/onButtonClick.js
@@ -1,3 +1,5 @@
+//for scrolling to the section on header buttons click
+//also could have been done through a href linking to that section or document.querySelector().scrollIntoView
 function onButtonClick(id) {
     const header = document.querySelector('.header').getBoundingClientRect();
     const section = document.getElementById(id).getBoundingClientRect();
---
+++ js/onScroll.js
@@ -0,0 +1,44 @@
+//for changing the header buttons texts on scrolling
+
+//getting the heights of the elements for conditioning
+let trendingHeight = document.querySelector("#Trending").getBoundingClientRect().height;
+let popularHeight = document.querySelector('[id = "Most Popular"]').getBoundingClientRect().height;
+let viewedHeight = document.querySelector('[id = "Most Viewed"]').getBoundingClientRect().height;
+let choiceHeight = document.querySelector('[id = "Your Choice"]').getBoundingClientRect().height;
+
+//function to change the buttons color according to the element on screen
+const scrollFunction = window.addEventListener("scroll", () => {
+  if (this.scrollY < trendingHeight) {
+    document.getElementById("buttonTrending").style.color = "black";
+    document.getElementById("buttonMostPopular").style.color = "white";
+    document.getElementById("buttonMostViewed").style.color = "white";
+    document.getElementById("buttonYourChoice").style.color = "white";
+  } else if (this.scrollY < trendingHeight + popularHeight - 1) { 
+    // did -1 to make most viewed black on click of most viewed else most popular gets the black color
+    document.getElementById("buttonTrending").style.color = "white";
+    document.getElementById("buttonMostPopular").style.color = "black";
+    document.getElementById("buttonMostViewed").style.color = "white";
+    document.getElementById("buttonYourChoice").style.color = "white";
+  } else if (this.scrollY < trendingHeight + popularHeight + viewedHeight) {
+    document.getElementById("buttonTrending").style.color = "white";
+    document.getElementById("buttonMostPopular").style.color = "white";
+    document.getElementById("buttonMostViewed").style.color = "black";
+    document.getElementById("buttonYourChoice").style.color = "white";
+  } else if (this.scrollY > trendingHeight + popularHeight + viewedHeight) {
+    document.getElementById("buttonTrending").style.color = "white";
+    document.getElementById("buttonMostPopular").style.color = "white";
+    document.getElementById("buttonMostViewed").style.color = "white";
+    document.getElementById("buttonYourChoice").style.color = "black";
+  }
+});
+
+//resizing the heights and calling the scoll function again if the size of the screen changes 
+//like if someone rotates the screen
+window.addEventListener('resize', () => {
+  trendingHeight = document.querySelector("#Trending").getBoundingClientRect().height;
+  popularHeight = document.querySelector('[id = "Most Popular"]').getBoundingClientRect().height;
+  viewedHeight = document.querySelector('[id = "Most Viewed"]').getBoundingClientRect().height;
+  choiceHeight = document.querySelector('[id = "Your Choice"]').getBoundingClientRect().height;
+
+  scrollFunction();
+});
